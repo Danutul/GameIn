9ba81ffae1330eb9b7d425c52854fabb5f8e9285
Dan <florutm.dan@gmail.com>
2019-02-11 17:41:49 +0200

    Implementing the window class

--- Project1/Window.cpp
+++ Project1/Window.cpp
@@ -6,7 +6,58 @@ Window::Window()
 {
 }
 
+Window::Window(const std::string & l_title, const sf::Vector2u & l_size)
+{
+}
+
 
 Window::~Window()
+{
+}
+
+void Window::BeginDraw()
+{
+}
+
+void Window::EndDraw()
+{
+}
+
+void Window::Update()
+{
+}
+
+bool Window::IsDone()
+{
+	return false;
+}
+
+bool Window::IsFullscreen()
+{
+	return false;
+}
+
+sf::Vector2u Window::GetWindowSize()
+{
+	return sf::Vector2u();
+}
+
+void Window::ToggleFullScreen()
+{
+}
+
+void Window::Draw(sf::Drawable & l_drawble)
+{
+}
+
+void Window::Setup(const std::string & l_title, const sf::Vector2u & l_size)
+{
+}
+
+void Window::Destroy()
+{
+}
+
+void Window::Create()
 {
 }
