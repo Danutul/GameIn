dde4a983a3be78109fb5e129d5f27c22f58afe09
Dan <florutm.dan@gmail.com>
2019-02-11 20:24:18 +0200

    snake h file implemented

--- Project1/Snake.h
+++ Project1/Snake.h
@@ -21,10 +21,45 @@ enum class Direction
 	Left,
 	Right
 };
+
 class Snake
 {
 public:
-	Snake();
+	Snake(int l_blockSize);
 	~Snake();
+
+	//Helper methods
+	void SetDirection(Direction l_dir);
+	Direction GetDirection();
+	int GetSpeed();
+	sf::Vector2i GetPosition();
+	int GetLives();
+	int GetScore();
+	void IncreaseScore();
+	bool HasLost();
+	
+	void Lose(); //hendle losing here
+	void ToggleLost();
+	void Extend(); // Grow the snake
+	void Reset(); //Reset the starting point
+	void Move();
+	void Tick(); //Update method
+	void Cut(int l_segments); //method for cutting snake
+	void Render(sf::RenderWindow& l_window);
+
+private:
+	void CheckCollision();
+
+	SnakeContainer m_snakeBody;
+
+	//segment vector
+	int m_size; //size of the graphics
+	Direction m_dir; //current direction
+	
+	int m_speed; 
+	int m_lives;
+	int m_score;
+	bool m_lost; //losing state
+	sf::RectangleShape m_bodyRerct;
 };
 
